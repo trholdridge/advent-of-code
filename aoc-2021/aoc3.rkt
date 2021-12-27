cf463a6df0072fce98ce2c5dfa993564e9b4f4ae
trholdridge <trholdridge@gmail.com>
2021-12-27 14:38:30 +0530

    day 3 pt 2

--- aoc-2021/aoc3.rkt
+++ aoc-2021/aoc3.rkt
@@ -1,8 +1,7 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname aoc3) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#lang racket
+
 (require 2htdp/batch-io)
-(require racket/string)
+(require test-engine/racket-tests)
 
 ; Input
 
@@ -49,13 +48,34 @@
 ; most-common-bit : Nat [List-of [List-of Nat]] -> Nat
 ; only returns 0 or 1
 (define (most-common-bit position lolon)
-  (round (/ (foldr + 0 (map (λ (lon) (list-ref position lon)) lolon))
+  (my-round (/ (foldr + 0 (map (λ (lon) (list-ref lon position)) lolon))
             (length lolon))))
 
+(define (my-round n)
+  (if (= n 0.5)
+      1
+      (round n)))
+
 ; keep-if-most-common : Nat [List-of [List-of Nat]] -> [List-of [List-of Nat]]
-(define (keep-if-most-common bit position lolon)
-  (filter (λ (lon) (= bit (list-ref position lon))) lolon))
+(define (keep-if-most-common position lolon)
+  (local [(define COMMON (most-common-bit position lolon))]
+    (filter (λ (lon) (= COMMON (list-ref lon position))) lolon)))
 
 ; keep-if-least-common : Nat [List-of [List-of Nat]] -> [List-of [List-of Nat]]
-(define (keep-if-least-common bit position lolon)
-  (filter (λ (lon) (not (= bit (list-ref position lon)))) lolon))
+(define (keep-if-least-common position lolon)
+  (local [(define COMMON (most-common-bit position lolon))]
+    (filter (λ (lon) (not (= COMMON (list-ref lon position)))) lolon)))
+
+; rating : [List-of [List-of Nat]] [keep if most/least common] -> [List-of Nat]
+(define (rating lolon func)
+  (foldl (λ (n filter-so-far)
+           (if (= 1 (length filter-so-far))
+               filter-so-far
+               (func n filter-so-far)))
+         lolon (build-list (length (first lolon)) values)))
+
+; Final steps
+
+(define (solve _)
+  (* (binary->decimal (first (rating INPUT keep-if-most-common)))
+     (binary->decimal (first (rating INPUT keep-if-least-common)))))
